339445cec57b7416e00031d78521ccad03bdb0ae
Ubuntu <ubuntu@ip-172-31-4-168.ap-northeast-1.compute.internal>
2020-06-21 15:09:26 +0000

    Finish 9.1.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_024739) do
+ActiveRecord::Schema.define(version: 2020_06_21_135148) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_06_20_024739) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
