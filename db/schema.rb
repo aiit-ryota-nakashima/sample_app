aa8e139088633a0c49b8d5bc7ab135e6570f4aa5
Ubuntu <ubuntu@ip-172-31-4-168.ap-northeast-1.compute.internal>
2020-06-25 20:49:58 +0000

    Add account activation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_135148) do
+ActiveRecord::Schema.define(version: 2020_06_25_185037) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,9 @@ ActiveRecord::Schema.define(version: 2020_06_21_135148) do
     t.string "password_digest"
     t.boolean "admin", default: false
     t.string "remember_digest"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
